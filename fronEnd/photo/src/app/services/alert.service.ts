0c290ed04314eaa88c4fa3ed971687623dfc3b00
NinoosMoshi <47070548+NinoosMoshi@users.noreply.github.com>
2021-05-28 11:25:07 -0500

    create enum and alert service

---
+++ fronEnd/photo/src/app/services/alert.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AlertType } from '../enum/alert-type.enum';
+import { Alert } from '../model/alert';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  public alerts: Subject<Alert> = new Subject();
+
+  constructor() { }
+
+  showAlert(message: string, alterType: AlertType): void {
+    const alert = new Alert(message, alterType);
+    this.alerts.next(alert);
+  }
+
+
+}
